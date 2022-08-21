6078ef4f28c9da41cc1292705e986feef47a5c2f
Dev Anand <mansion3058@gmail.com>
2022-08-21 13:35:41 +0530

    Update Expression Solver.py

--- Expression Solver.py
+++ Expression Solver.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Expression Solver
-
-# In[32]:
-
 
 inp=input("Enter the Expression: ")
 fotp=""
@@ -13,15 +7,17 @@ r=0
 n=[]
 for a in range(0,len(inp)):
     if inp[a].isdigit()==False:
-        if inp[a-(len(inp)-1)].isdigit()==True and a-(len(inp)-1)!=0:
+        if inp[a-(len(inp)-1)].isdigit()==True\ 
+        and a-(len(inp)-1)!=0:
             fotp+=inp[a]
         else:
-            break                             # Only Special Characters
+            break              # Only Special Characters
 for a in range(0,len(fotp)):
-    if ord(fotp[a])==42 or ord(fotp[a])==43 or ord(fotp[a])==45 or ord(fotp[a])==47:
+    if ord(fotp[a])==42 or ord(fotp[a])==43\
+    or ord(fotp[a])==45 or ord(fotp[a])==47:
         notp+=fotp[a] 
     else:
-        break            # Only Arithmetic Operators except "%"
+        break    # Only Arithmetic Operators except "%"
 k=0
 dgt=""
 for a in range(0,len(notp)+1):
@@ -31,7 +27,7 @@ for a in range(0,len(notp)+1):
         k+=1
     k+=1
     dgt=int(dgt)
-    n.append(dgt)                           # Only Operands Figures
+    n.append(dgt)        # Only Operands Figures
 k=0
 r=n[0]
 for a in range(0,len(notp)):
@@ -43,5 +39,4 @@ for a in range(0,len(notp)):
         r=r*n[a+1]
     elif ord(notp[a])==47:
         r=r/n[a+1]
-print(r)
-
+print(round(r,2))
